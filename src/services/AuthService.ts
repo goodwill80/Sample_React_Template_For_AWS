e63c6e98d0ffa4ab3afea286f920adc339b09eaa
Ho Kit Jonathan <kitjonathan.ho@stengg.com>
2024-04-05 10:24:32 +0800

    added auth service and deployed on aws

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,37 +1,42 @@
-import { SignInOutput, signIn } from '@aws-amplify/auth';
-import { Amplify } from 'aws-amplify';
-
-const userPoolId = '';
-const UserPoolClientId = '';
-const identityPoolId = '';
+import { SignInOutput, fetchAuthSession, signIn } from "@aws-amplify/auth";
+import { Amplify } from "aws-amplify";
+import { AuthStack } from "../../../AWS_CDK_STARTER_API/outputs.json";
 
 // Initialise Amplify API to communicate with AWS cognito - by passing in userPoolID and userIdentityPoolId
 Amplify.configure({
   Auth: {
     Cognito: {
-      userPoolId: userPoolId,
-      userPoolClientId: UserPoolClientId,
-      identityPoolId: identityPoolId,
+      userPoolId: AuthStack.SpaceUserPoolId,
+      userPoolClientId: AuthStack.SpaceUserPoolClientId,
+      identityPoolId: AuthStack.SpaceIdentityPoolId,
     },
   },
 });
 
 export class AuthService {
+  private user: string | undefined;
   public async login(
     username: string,
     password: string
   ): Promise<object | undefined> {
+    // Sign in
     const result = (await signIn({
       username,
       password,
       options: {
-        authFlowType: 'USER_PASSWORD_AUTH',
+        authFlowType: "USER_PASSWORD_AUTH",
       },
     })) as SignInOutput;
+    // Get user details from session
+    const session = await fetchAuthSession();
+
+    if (session.tokens?.signInDetails?.loginId) {
+      this.user = session.tokens?.signInDetails?.loginId as string;
+    }
     return result;
   }
 
   public getUserName() {
-    return 'some user';
+    return this.user;
   }
 }
